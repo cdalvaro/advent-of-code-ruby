39bede533659db525e23df2546633da0ae84c7b4
Carlos Álvaro <carlos@cdalvaro.io>
2023-12-24 17:06:15 +0100

    Advent of Code 2023 - Day 4
    - Solve day 4 puzzle

---
+++ test/puzzles/2023/day04/day04_test.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require "puzzles/2023/day04"
+
+require "minitest/autorun"
+
+module AdventOfCode
+  module Test
+    module Puzzles2023
+      module Day04
+        ##
+        # Tests Day 4 - Part 1
+        class Part1Test < Minitest::Test
+          def setup
+            # Do nothing
+          end
+
+          def teardown
+            # Do nothing
+          end
+
+          def test_points_test_data_set
+            input_file = "#{File.dirname(__FILE__)}/test_data.txt"
+            puzzle = AdventOfCode::Puzzles2023::Day04::Part1.new(file: input_file)
+
+            assert_equal [8, 2, 2, 1, 0, 0], puzzle.points
+          end
+
+          def test_answer_test_data_set
+            input_file = "#{File.dirname(__FILE__)}/test_data.txt"
+            puzzle = AdventOfCode::Puzzles2023::Day04::Part1.new(file: input_file)
+
+            assert_equal 13, puzzle.answer
+          end
+
+          def test_answer_input_set
+            input_file = "#{File.dirname(__FILE__)}/input.txt"
+            puzzle = AdventOfCode::Puzzles2023::Day04::Part1.new(file: input_file)
+
+            assert_equal 22_897, puzzle.answer
+          end
+        end
+
+        ##
+        # Tests Day 4 - Part 2
+        class Part2Test < Minitest::Test
+          def setup
+            # Do nothing
+          end
+
+          def teardown
+            # Do nothing
+          end
+
+          def test_total_cards_test_set
+            input_file = "#{File.dirname(__FILE__)}/test_data.txt"
+            puzzle = AdventOfCode::Puzzles2023::Day04::Part2.new(file: input_file)
+
+            assert_equal([1, 2, 4, 8, 14, 1], puzzle.total_cards.map { |c| c[:copies] })
+          end
+
+          def test_answer_test_data_set
+            input_file = "#{File.dirname(__FILE__)}/test_data.txt"
+            puzzle = AdventOfCode::Puzzles2023::Day04::Part2.new(file: input_file)
+
+            assert_equal 30, puzzle.answer
+          end
+
+          def test_answer_input_set
+            input_file = "#{File.dirname(__FILE__)}/input.txt"
+            puzzle = AdventOfCode::Puzzles2023::Day04::Part2.new(file: input_file)
+
+            assert_equal 5_095_824, puzzle.answer
+          end
+        end
+      end
+    end
+  end
+end
