1c46ace198d00c3df6dce440a980bfb55e360201
Carlos Álvaro <carlos@cdalvaro.io>
2024-12-25 18:15:20 +0100

    Advent of Code 2024 - Day 4

---
+++ lib/puzzles/2024/day04/day04.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "part1"
+require_relative "part2"
+
+module AdventOfCode
+  module Puzzles2024
+    ##
+    # {include:file:lib/puzzles/2024/day04/README.md}
+    module Day04
+    end
+  end
+end
