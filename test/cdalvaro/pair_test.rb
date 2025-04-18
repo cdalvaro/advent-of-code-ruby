70e0c64f1bcd57427741487d6c1c605c6b46753e
Carlos Álvaro <carlos@cdalvaro.io>
2025-04-18 10:54:35 +0200

    chore(lib): Improve Pair coverage

--- test/cdalvaro/pair_test.rb
+++ test/cdalvaro/pair_test.rb
@@ -4,30 +4,38 @@ require "test_helper"
 require "cdalvaro/pair"
 
 module Cdalvaro
-  class PairTest < ActiveSupport::TestCase
-    def setup
-      # Do nothing
-    end
+  module Test
+    class PairTest < ActiveSupport::TestCase
+      def setup
+        # Do nothing
+      end
 
-    def teardown
-      # Do nothing
-    end
+      def teardown
+        # Do nothing
+      end
 
-    def test_readers
-      pair = Pair.new("Hello", :world)
+      def test_readers
+        pair = Pair.new("Hello", :world)
 
-      assert_equal "Hello", pair.first
-      assert_equal :world, pair.second
-    end
+        assert_equal "Hello", pair.first
+        assert_equal :world, pair.second
+      end
 
-    def test_writers
-      pair = Pair.new("Hello", :world)
+      def test_writers
+        pair = Pair.new("Hello", :world)
 
-      pair.first = :earth
-      pair.second = "Goodbye"
+        pair.first = :earth
+        pair.second = "Goodbye"
 
-      assert_equal :earth, pair.first
-      assert_equal "Goodbye", pair.second
+        assert_equal :earth, pair.first
+        assert_equal "Goodbye", pair.second
+      end
+
+      def test_to_s
+        pair = Pair.new("Hello", :world)
+
+        assert_equal "[Hello, :world]", pair.to_s
+      end
     end
   end
 end
